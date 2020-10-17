65a1d073151e0f19b6a246b8970316081422c44d
Maikel Jesús Spranger Hierro <maikelsp@ucm.es>
2020-10-18 00:05:35 +0200

    added quest system

---
+++ Assets/Scripts/Quests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quests : MonoBehaviour
+{
+    public string shortDescription;
+    public string description;
+    public Actions actionNeeded; 
+    public bool condition;
+
+
+    private void Start()
+    {
+        condition = false;
+    }
+}
