938b40eb9c53202034f01209c65bb13671adff2f
Maikel Jesús Spranger Hierro <maikelsp@ucm.es>
2020-10-18 11:42:33 +0200

    Added gauge based on steps sound

---
+++ Assets/Scripts/Gauge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gauge : MonoBehaviour
+{
+
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        slider.value = 0;
+    }
+
+    public void setGauge(float value)
+    {
+        slider.value += value;
+    }
+}
