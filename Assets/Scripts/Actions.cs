e00d020e6bb996547beaa9beb32d1c3b767b7b61
Maikel Jesús Spranger Hierro <maikelsp@ucm.es>
2020-10-21 17:14:22 +0200

    added vuvuzela quest

--- Assets/Scripts/Actions.cs
+++ Assets/Scripts/Actions.cs
@@ -6,5 +6,6 @@ public enum Actions
 {
     PickUp,
     FillUp,
-    Splash
+    Splash,
+    Blow
 }
--- Assets/Scripts/CharacterActions.cs
+++ Assets/Scripts/CharacterActions.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class CharacterActions : MonoBehaviour
 {
 
-    private Collider2D collisionItem;
-    private Collider2D item;
+    private GameObject collisionItem;
+    private GameObject item;
     private bool collide = false;
     private bool picked = false;
     private bool fillUp = false;
@@ -16,10 +16,9 @@ public class CharacterActions : MonoBehaviour
     public Button spaceButton;
     public GameObject selectedItem;
 
-    public Sprite bigButton;
-    public Sprite bigButtonClicked;
-
     private GameObject questTab;
+
+    public GameObject balloonVuvuzela;
 
     private void Awake()
     {
@@ -33,7 +32,7 @@ public class CharacterActions : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            spaceButton.image.sprite = bigButtonClicked;
+            spaceButton.image.sprite = gameObject.GetComponent<Movement>().bigButtonClicked;
 
             // Pick up item
             if (!picked)
@@ -41,13 +40,13 @@ public class CharacterActions : MonoBehaviour
                 if (collide)
                 {
                     item = collisionItem;
-                    img.sprite = collisionItem.GetComponent<SpriteRenderer>().sprite;
+                    img.sprite = item.GetComponent<SpriteRenderer>().sprite;
                     img.SetNativeSize();
-                    img.transform.localScale = collisionItem.transform.localScale;
+                    img.transform.localScale = item.transform.localScale;
                     img.enabled = true;
-                    collisionItem.transform.parent = gameObject.transform;
-                    collisionItem.transform.position = gameObject.transform.position;
-                    collisionItem.GetComponent<SpriteRenderer>().enabled = false;
+                    item.transform.parent = gameObject.transform;
+                    item.transform.position = gameObject.transform.position;
+                    item.GetComponent<SpriteRenderer>().enabled = false;
                     picked = true;
                     if (filled)
                         fillUp = true;
@@ -56,14 +55,55 @@ public class CharacterActions : MonoBehaviour
             }
             else
             {
-                // Leave item on the floor
-                if (collisionItem != null)
+                if (collide)
                 {
+                    if ((item.CompareTag("Vuvuzela") && collisionItem.CompareTag("Balloon")) || (item.CompareTag("Balloon") && collisionItem.CompareTag("Vuvuzela")))
+                    {
+                        GameObject oldItem = item;
+
+                        item = Instantiate(balloonVuvuzela, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity, gameObject.transform);
+                        img.sprite = item.GetComponent<SpriteRenderer>().sprite;
+                        img.SetNativeSize();
+                        img.transform.localScale = item.transform.localScale;
+                        img.enabled = true;
+                        item.transform.parent = gameObject.transform;
+                        item.transform.position = gameObject.transform.position;
+                        item.GetComponent<SpriteRenderer>().enabled = false;
+
+                        Destroy(oldItem);
+                        Destroy(collisionItem);
+                        QuestItem(Actions.PickUp);
+                    }
+                    else
+                    {
+                        if (collisionItem != item)
+                        {
+                            GameObject currentItem = item.gameObject;
+
+                            item = collisionItem;
+                            img.sprite = item.GetComponent<SpriteRenderer>().sprite;
+                            img.SetNativeSize();
+                            img.transform.localScale = item.transform.localScale;
+                            img.enabled = true;
+                            item.transform.parent = gameObject.transform;
+                            item.transform.position = gameObject.transform.position;
+                            item.GetComponent<SpriteRenderer>().enabled = false;
+
+                            currentItem.GetComponent<SpriteRenderer>().enabled = true;
+                            currentItem.transform.parent = null;
+                            currentItem.transform.position = new Vector3(currentItem.transform.position.x, currentItem.transform.position.y, 1);
+
+                            QuestItem(Actions.PickUp);
+                        }
+                    }
+                }
+                else
+                // Leave item on the floor
+                {
+                    item.GetComponent<SpriteRenderer>().enabled = true;
+                    item.transform.parent = null;
+                    item.transform.position = new Vector3(item.transform.position.x, item.transform.position.y, 1);
                     item = null;
-                    collisionItem.GetComponent<SpriteRenderer>().enabled = true;
-                    collisionItem.transform.parent = null;
-                    collisionItem.transform.position = new Vector3(collisionItem.transform.position.x, collisionItem.transform.position.y, 1);
-                    collisionItem = null;
                     img.sprite = null;
                     img.enabled = false;
                     picked = false;
@@ -75,7 +115,7 @@ public class CharacterActions : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            spaceButton.image.sprite = bigButton;
+            spaceButton.image.sprite = gameObject.GetComponent<Movement>().bigButton;
         }
 
         if (fillUp)
@@ -89,14 +129,14 @@ public class CharacterActions : MonoBehaviour
         }
     }
 
-    public Collider2D getPicked()
+    public GameObject getPicked()
     {
         return collisionItem;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Diary")
+        if (collision.CompareTag("Diary"))
         {
             Destroy(collision.gameObject);
             questTab.GetComponent<QuestTab>().OpenQuestTab();
@@ -105,23 +145,28 @@ public class CharacterActions : MonoBehaviour
         if (collision.gameObject.layer == 9)
         {
             collide = true;
-            collisionItem = collision;
+            collisionItem = collision.gameObject;
         }
 
-        if (collision.gameObject.tag == "Sink" && collisionItem != null)
+        if (collision.gameObject.CompareTag("Sink") && collisionItem != null)
         {
-            if(collisionItem.tag == "Bucket" && picked)
+            if(collisionItem.CompareTag("Bucket") && picked)
             {
                 collisionItem.GetComponent<Animator>().SetBool("filled", true);
                 fillUp = true;
             }
         }
 
-        if(collision.gameObject.tag == "Hit" && item != null)
+        if(collision.gameObject.CompareTag("Hit") && item != null)
         {
-            if(item.tag == "Bucket" && filled)
+            if(item.gameObject.CompareTag("Bucket") && filled)
             {
                 QuestItem(Actions.Splash);
+            }
+
+            if (item.gameObject.CompareTag("BalloonVuvuzela"))
+            {
+                QuestItem(Actions.Blow);
             }
         }
     }
